039a704491a30d87c373232396fa5d185475a22e
user <tombarrett@siu.edu>
2016-10-23 16:51:03 -0500

    added ph, needs testing

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
+// pH Sensor Initalization
+SoftwareSerial phSerial(2,3);
+String pH = "";
+bool phComplete = false;
+
+// Pin Initialization
 void setup(){
     Serial.begin(9600);
+    phSerial.begin(9600);
+    pH.reserve(30);
 }
 
+// Data RX/TX
 void loop(){
     // Wait for go ahead 
     while(true){
@@ -11,10 +21,28 @@ void loop(){
             break;
         delay(500);
     }
+
     // Collect Data
+    if(phSerial.available()){
+        char in = (char)phSerial.read();
+        pH += in;
+        if(in == '\r'){
+            phComplete = true;
+        }
+    }
+
+    // Format Data
     String data;
+    
     data = "55,";
+    
+    if(phComplete)
+        data += pH;
+    else
+        data += "0,";
+    
     data += "20,";
+    
     data += String(10) + ",";
     
     // Send data Serially
