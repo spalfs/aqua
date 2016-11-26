480bea8debe1b1310de0ab24a70d9304a88276ed
Tom Barrett <tombarrett@siu.edu>
2016-11-26 04:59:41 +0000

    finished matplotlib

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -83,6 +83,7 @@ def insert(d):
     try:
         c.execute('''CREATE TABLE data
         (date TEXT,
+        time  TEXT,
         rtemp REAL,
         humid REAL,
         wtemp REAL,
@@ -96,24 +97,29 @@ def insert(d):
     except:
         print("data.db already created")
 
-    now = datetime.now().replace(microsecond=0)
+    dnow = datetime.now().date()
+    tnow = datetime.now().time()
 
     c.execute('''INSERT INTO data VALUES
-            ('%s',   %f,   %f,   %f,   %d,   %d,   %d,   %d,   %d,   %f)''' % (
-              now, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8]))
+            ('%s',  '%s',   %f,   %f,   %f,   %d,   %d,   %d,   %d,   %d,   %f)''' % (
+              dnow, tnow, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8]))
 
     conn.commit()
     conn.close()
 
-def plot():
-    data  = "wlevl"
-    begin = "2016-11-26"
-    end   = "2016-11-26"
+def plot(data,begin,end):
+
+    if(len(data)==0):
+        return 1
+
+    data = str(data).replace("'","")
+    data = data.replace("[","")
+    data = data.replace("]","")
 
     conn = sqlite3.connect('/mnt/data.db')
     c = conn.cursor()
 
-    c.execute("SELECT date FROM data WHERE date LIKE '%%%s%%'" % (begin))
+    c.execute("SELECT time FROM data WHERE date LIKE '%%%s%%'" % (begin))
     x = c.fetchall()
     c.execute("SELECT %s FROM data WHERE date LIKE '%%%s%%'" % (data,begin))
     y = c.fetchall()
@@ -126,14 +132,25 @@ def plot():
     data = data.split(',')
     for i in range(len(data)):
         ny = [z[i] for z in y]
-        plt.plot(x,ny,label=data[i])
+        plt.plot_date(x,ny,label=data[i])
 
     plt.legend()
-    ax.set_xlim(x[0][0],x[-1][0])
-    plt.gcf().autofmt_xdate()
 
     plt.savefig('/home/pi/Desktop/aqua/static/plot_bmh.png',bbox_inches='tight')
 
     conn.close()
+
+def getUniqueDates():
+    conn = sqlite3.connect('/mnt/data.db')
+    c = conn.cursor()
+
+    c.execute("SELECT DISTINCT date FROM data")
+    x = c.fetchall()
+    conn.close()
+    xL = []
+    for i in x:
+        xL.append((i[0],i[0]))
+
+    return xL
 
 
--- server.py
+++ server.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template
-from flask.ext.wtf import Form
-from wtforms import SelectMultipleField, widgets, SubmitField
-from scripts.scripts import createGraphs, createSystemGraphs, getIP
+from flask.ext.wtf import FlaskForm
+from wtforms import SelectMultipleField, widgets, SubmitField, SelectField
+from scripts.scripts import createGraphs, createSystemGraphs, getIP, getUniqueDates, plot
 
 SECRET_KEY = 'dev'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+def getFields():
+    return [("rtemp,wtemp","Temperature")
+           ,("humid","Humidity")
+           ,("rlite,llite,blite,tlite","Lighting")
+           ,("ph","pH")
+           ,("wlevl","Water Level")]
+
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
 
-class SimpleForm(Form):
-    rField = MultiCheckboxField('Parameters',choices=[("rt","RoomTemp"),("wt","WaterTemp"),("pH","pH"),("wl","Water Level")])
+class SimpleForm(FlaskForm):
+    x = getFields()
+    rField = MultiCheckboxField('Parameters',choices=x)
+    x = getUniqueDates()
+    sField = SelectField('Start', choices=x)
+    eField = SelectField('End', choices=x)
 
 ip = getIP("wlan0")
 
@@ -34,7 +45,8 @@ def recent():
 def all():
     form = SimpleForm()
     if form.validate_on_submit():
-        print (form.rField.data)
+        plot(form.rField.data, form.sField.data, form.eField.data)
+        return render_template('all.html', layout = "all", ip = ip, form = form)
     else:
         print (form.errors)
     return render_template('all.html', layout = "all", ip = ip, form = form)
